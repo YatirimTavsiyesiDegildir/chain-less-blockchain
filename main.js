fec1e4c2840aa40551595f280caf5cc7f81b8651
Dogu <dogudeniz.ugur@gmail.com>
2021-03-07 03:44:59 +0300

    Block data name renamed to title

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ var MessageType = {
 };
 
 var getGenesisBlock = () => {
-    return new Block(0, "0", 0, "{type:'report', data:{id: 0,name: 'First data!', description: 'This is a test data', place: 'Milky way galaxy.', date: 0}}", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
+    return new Block(0, "0", 0, "{type:'report', data:{id: 0,title: 'First data!', description: 'This is a test data', place: 'Milky way galaxy.', date: 0}}", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
 var blockchain = [getGenesisBlock()];
